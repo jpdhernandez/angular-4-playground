4d124b5efd8e9176c6d72aea358af654dec6482e
Julian Philip Hernandez <jpdhernandez@gmail.com>
2017-05-25 11:40:29 -0400

    Play with local references in the template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   // @output will allow parent components listen to events created with new eventemitter
   @Output('srvCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,16 +17,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
   
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
